cc439a13901cfb22c0034cb782979d496b0c5748
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-12-17 19:55:20 +0100

    Done

--- Uppfinnaren/Controllers/HomeController.cs
+++ Uppfinnaren/Controllers/HomeController.cs
@@ -26,10 +26,7 @@ namespace Uppfinnaren.Controllers
         {
             return View();
         }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
