a5cb9c66ef2a8c3052d8fd129a75b49e9ef7f241
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-12-20 17:08:52 +0100

    Har lagt till databas-kopplingen med InMemoryDatabase samt lagt till några fler alster som ska finnas på varje sida.

---
+++ Uppfinnaren/Models/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Uppfinnaren.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProductRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Product> AllProducts
+        {
+            get 
+            { 
+                return _appDbContext.Products.Include(c => c.Category); 
+            }
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return _appDbContext.Products.FirstOrDefault(p => p.ProductId == productId);
+        }
+    }
+}
--- Uppfinnaren/Startup.cs
+++ Uppfinnaren/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,8 +25,10 @@ namespace Uppfinnaren
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, MockProductRepository>();
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("products"));
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
         }
 
