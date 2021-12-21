cd944b0e786b702d51da7ff90b02a40901c30bf5
jajo21 <89775394+jajo21@users.noreply.github.com>
2021-12-21 19:55:19 +0100

    Enhetstester skapade och fixade. Behöver möjligtvis någon ändring.

--- Uppfinnaren/Models/MockCategoryRepository.cs
+++ Uppfinnaren/Models/MockCategoryRepository.cs
@@ -10,7 +10,8 @@ namespace Uppfinnaren.Models
                 new Category{CategoryId=1, CategoryName="Tavlor", Description="Vackra tavlor."},
                 new Category{CategoryId=2, CategoryName="Skulpturer", Description="Fantastiska skulpturer."},
                 new Category{CategoryId=3, CategoryName="Smycken", Description="Underbara smycken."},
-                new Category{CategoryId=4, CategoryName="Verktyg", Description="Funktionella uppfinningar som fungerar till och för olika verktyg i livet."}
+                new Category{CategoryId=4, CategoryName="Verktyg", Description="Funktionella uppfinningar som fungerar till och för olika verktyg i livet."},
+                new Category{CategoryId=5, CategoryName="Möbler", Description ="Uppfinningsrika möbler"}
             };
     }
 }
